67bd369c601a7f88427e2e41bdb99fcdbf5c68ec
Estevão Santos Ribeiro <estevaosantosribeiro@gmail.com>
2026-05-14 03:50:41 -0300

    Auto-generated commit

---
+++ chunk-OSS4H7QR.js
@@ -0,0 +1,1 @@
+import"./chunk-7CGTOI24.js";var o=[{path:"",loadComponent:()=>import("./chunk-5UXXNITT.js").then(e=>e.HomeComponent)}];export{o as HOME_ROUTES};
--- chunk-U7PNNMCI.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-7CGTOI24.js";var o=[{path:"",loadComponent:()=>import("./chunk-LXG2O3FC.js").then(e=>e.HomeComponent)}];export{o as HOME_ROUTES};
